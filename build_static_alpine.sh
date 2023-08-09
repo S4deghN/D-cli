39ff4aedecef90b898702e71af55a5a6ff438cdc
s4 <sadegh.nobakhti@gmail.com>
2023-08-09 19:52:07 +0330

    compiled dynamic

--- build_static_alpine.sh
+++ build_static_alpine.sh
@@ -1,6 +1,7 @@
-#!/bin/ash
+#!/bin/sh
 
-apk update
-apk add build-base cmake openssl openssl-dev openssl-libs-static linux-headers
+# apk update
+# apk add build-base cmake openssl openssl-dev openssl-libs-static linux-headers
+# openssl static library doesn't exist on arch linux, so turn of in that case.
 cmake -B./build -DCMAKE_BUILD_TYPE=RELEASE -DSTATIC_BINARY=true .
 make -C ./build -j $(nproc)
